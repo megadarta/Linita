b20f832dc1ffa6c890962385ec4f93f6a90b752a
bbrillianta <brilliantabintangvirgantara@gmail.com>
2021-04-04 19:50:33 +0700

    add production environtment

--- server/configs/session-config.js
+++ server/configs/session-config.js
@@ -1,11 +1,12 @@
 const MongoStore = require('connect-mongo');
+const { IS_PROD } = require('./env-config');
 
 const sessionSetting = {
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
-    cookie: { secure: false }    
+    cookie: { secure: IS_PROD }    
 }
 
 module.exports = sessionSetting;
