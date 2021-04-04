8cdba0b943411f4021028487e2f429a79327a8b0
bbrillianta <brilliantabintangvirgantara@gmail.com>
2021-04-04 20:00:57 +0700

    test for deployment

--- server/configs/session-config.js
+++ server/configs/session-config.js
@@ -6,7 +6,7 @@ const sessionSetting = {
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
-    cookie: { secure: IS_PROD }    
+    cookie: { secure: false }    
 }
 
 module.exports = sessionSetting;
