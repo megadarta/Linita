14f847a2edd16e1efc687dd2dcf1a9f51b836444
mega darta <mega28102001@gmail.com>
2021-04-08 11:20:40 +0800

    kamispagi

--- client/src/component/Carakonsultasi.js
+++ client/src/component/Carakonsultasi.js
@@ -9,31 +9,31 @@ function Carakonsultasi() {
     
             <div className="container">
             <div className="row justify-content-center">
-            <div class="card card-konsul" style={{width: "18rem"}}>
+            <div class="card card-konsul">
                 <img class="card-img-top p-5" src="./asset/location.png" alt="Card image cap"></img>
                     <div class="card-body">
                         <p class="card-text text-card-konsul">Kamu bisa melakukan konseling di mana saja kamu berada.</p>
                     </div>
             </div>
-            <div class="card card-konsul" style={{width: "18rem"}}>
+            <div class="card card-konsul">
                 <img class="card-img-top p-5" src="./asset/clock.png" alt="Card image cap"></img>
                     <div class="card-body">
                         <p class="card-text text-card-konsul">Waktu layanan konsultasi yang fleksibel.</p>
                     </div>
             </div>
-            <div class="card card-konsul" style={{width: "18rem"}}>
+            <div class="card card-konsul">
                 <img class="card-img-top p-5" src="./asset/verified.png" alt="Card image cap"></img>
                     <div class="card-body">
                         <p class="card-text text-card-konsul">Layanan konsultasi dengan psikolog atau psikiater berkompeten dan berpengalaman.</p>
                     </div>
             </div>
-            <div class="card card-konsul" style={{width: "18rem"}}>
+            <div class="card card-konsul">
                 <img class="card-img-top p-5" src="./asset/problem-solving.png" alt="Card image cap"></img>
                     <div class="card-body">
                         <p class="card-text text-card-konsul">Membantu menyelesaikan masalahmu dengan solusi yang solutif.</p>
                     </div>
             </div>
-            <div class="card card-konsul" style={{width: "18rem"}}>
+            <div class="card card-konsul">
                 <img class="card-img-top p-5" src="./asset/heartbeat.png" alt="Card image cap"></img>
                     <div class="card-body">
                         <p class="card-text text-card-konsul">DIjamin anda akan jauh merasa lebih baik secara mental.</p>
