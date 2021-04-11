d046b3fd7db94b8caffcd439743f748db0578ff3
bayuhnm <bayuhidayahnurm@gmail.com>
2021-04-11 15:02:23 +0700

    Create Card and Hero

---
+++ client/src/component/Cardcerita.js
@@ -0,0 +1,64 @@
+import '../css/Cardcerita.css';
+import Hero from './Herostories.js';
+
+function Cardcerita() {
+  return (
+    <div className="custom-card-cerita flex-column">
+    <Hero />
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+            <div className="card card-cerita">
+                <div className="card-body">
+                    <h4 className="card-text text-card-cerita text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a ipsum aliquet</h4>
+                    <h6 className="card-text text-card-penulis text-left">by Mega Mendung</h6>
+                </div>
+            </div>
+            <div className="card card-cerita">
+                <div className="card-body">
+                    <h4 className="card-text text-card-cerita text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a ipsum aliquet</h4>
+                    <h6 className="card-text text-card-penulis text-left">by Mega Mendung</h6>
+                </div>
+            </div>
+            <div className="card card-cerita">
+                <div className="card-body">
+                    <h4 className="card-text text-card-cerita text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a ipsum aliquet</h4>
+                    <h6 className="card-text text-card-penulis text-left">by Mega Mendung</h6>
+                </div>
+            </div>
+            <div className="card card-cerita">
+                <div className="card-body">
+                    <h4 className="card-text text-card-cerita text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a ipsum aliquet</h4>
+                    <h6 className="card-text text-card-penulis text-left">by Mega Mendung</h6>
+                </div>
+            </div>
+            <div className="card card-cerita">
+                <div className="card-body">
+                    <h4 className="card-text text-card-cerita text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a ipsum aliquet</h4>
+                    <h6 className="card-text text-card-penulis text-left">by Mega Mendung</h6>
+                </div>
+            </div>
+            <div className="card card-cerita">
+                <div className="card-body">
+                    <h4 className="card-text text-card-cerita text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a ipsum aliquet</h4>
+                    <h6 className="card-text text-card-penulis text-left">by Mega Mendung</h6>
+                </div>
+            </div>
+            <div className="card card-cerita">
+                <div className="card-body">
+                    <h4 className="card-text text-card-cerita text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a ipsum aliquet</h4>
+                    <h6 className="card-text text-card-penulis text-left">by Mega Mendung</h6>
+                </div>
+            </div>
+            <div className="card card-cerita">
+                <div className="card-body">
+                    <h4 className="card-text text-card-cerita text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a ipsum aliquet</h4>
+                    <h6 className="card-text text-card-penulis text-left">by Mega Mendung</h6>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+  export default Cardcerita;
