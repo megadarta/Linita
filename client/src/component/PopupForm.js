a266ec5a6a4accd9d82f23c21ce94e055e3ac9e9
bayuhnm <bayuhidayahnurm@gmail.com>
2021-04-15 20:48:07 +0700

    fix Close button

--- client/src/component/PopupForm.js
+++ client/src/component/PopupForm.js
@@ -7,7 +7,8 @@ function PopupForm() {
             <label for="modal-btn">TULIS CERITA<i class="uil uil-expand-arrows"></i></label>
                 <div class="modal">		
                     <div class="modal-wrap">
-                        <h3 class="judul">LOREM IPSUM DOLOR</h3>
+                        <h3 class="judul">ATURAN DAN KETENTUAN CERITA</h3>
+                        <div class="title">I. Ketentuan Umum Bagi Pengguna :</div>
                         <p>1. Pengguna dengan ini menyatakan bahwa pengguna adalah subjek hukum yang cakap dan mampu untuk mengikatkan diri dalam perjanjian.
                             <br></br>
                             2. Setiap pengguna sepakat untuk tidak menyebarluaskan berita atau informasi yang tidak benar (hoaks) di dalam platform Linita
@@ -23,7 +24,21 @@ function PopupForm() {
                             7. Tulisan pengguna tidak melanggar hukum atau peraturan apa pun yang berlaku.
                             <br></br>
                             8. Pengguna bertanggung jawab untuk menjaga kerahasiaan dan keamanan atas nama username dan password serta bertanggung jawab sepenuhnya atas segala kegiatan yang dilakukan oleh atau diatasnamakan nama akun Pengguna.
+                            <br></br>
+                            <br></br>
+                            Setiap penggunaan yang melanggar ketentuan di atas dapat mengakibatkan, antara lain, penghentian hak Pengguna untuk menggunakan paltform Linita serta pelaporan terhadap pihak yang berwenang .  
+                            <br></br>
+                            <br></br>
                         </p>
+                        <div class="title">II. Tanggung Jawab Kami :</div>
+                          <p>
+                            1. Kewajiban kami hanyalah sebatas penyediaan platform linita.
+                            <br></br>
+                            2. Bentuk kerugian yang dikarenakan tindakan dari pengguna yang melanggar ketentuan ini menjadi tanggung jawab pengguna.
+                            <br></br>
+                            3. Tidak akan menyebarkan identitas pribadi pengguna kepada siapapun tanpa persetujuan.
+                            <br></br>
+                          </p>
                         <div>
                             <div class="form-group">
                                 <label for="exampleInputName">NAMA LENGKAP*</label><br></br>
@@ -40,7 +55,9 @@ function PopupForm() {
                                 <input type="radio" class="form-pilihan" name="option" value="no"></input>		  
                                 <a for="anonymous">Tidak diizinkan, samarkan nama saya.</a><br></br>  	
                             </div>
+                            <div class="container rules-button">
                             <button type="submit" className="btn-color btn-rules">Submit</button>	
+                            </div>
                         </div>
                     </div>	
                 </div> 
