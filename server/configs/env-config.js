245f7d1c0dbd202a0093a3e070ea45cf2bd63317
bbrillianta <brilliantabintangvirgantara@gmail.com>
2021-04-16 01:35:04 +0700

    testing host server pt 4

--- server/configs/env-config.js
+++ server/configs/env-config.js
@@ -1,4 +1,4 @@
 IS_PROD=process.env.NODE_ENV === 'production' ? true : false;
-CLIENT= IS_PROD ? 'https://linita-unity.netlify.app' : 'http://localhost:3000';
+CLIENT=['https://linita-unity.netlify.app', 'http://localhost:3000'];
 
 module.exports = { CLIENT, IS_PROD };
