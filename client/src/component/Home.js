b6e8f8e79f927945282f64e0aab0fafe578f9026
bayuhnm <bayuhidayahnurm@gmail.com>
2021-04-19 11:03:15 +0700

    create top section of homepage

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -3,8 +3,11 @@ import '../css/Home.css';
 function Home() {
     return (
         <div id="top">
-        <div class="container-fluid">
-            
+        <div class="container-fluid konten-atas">
+            <div class="konten-kiri">
+                <div class="background"/>
+                <img class="image2" src="asset/slide01.png" />
+            </div>
         </div>
         </div>
     );
